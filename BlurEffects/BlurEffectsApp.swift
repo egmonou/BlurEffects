6e9edc8360a7e26e6fb8c1481544ec63e65aca23
Nour <mohamednour25@gmail.com>
2024-09-12 08:24:13 +0300

    Initial Commit

---
+++ BlurEffects/BlurEffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlurEffectsApp.swift
+//  BlurEffects
+//
+//  Created by administrator on 12/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BlurEffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
